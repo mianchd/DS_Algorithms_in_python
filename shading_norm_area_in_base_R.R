c133427e491c0cd2e74f29ca2551fba9c140f330
Mian <mianchd@gmail.com>
2019-12-09 11:24:25 -0500

    organised

--- shading_norm_area_in_base_R.R
+++
@@ -1,6 +0,0 @@
-shade.norm <- function(x1, x2){
-    cord.x <- c(x1, seq(x1, x2, 0.01), x2)
-    cord.y <- c(-0.012, dnorm(seq(x1, x2, 0.01)), -0.012)
-    polygon(cord.x, cord.y, col="lightblue") 
-  }
-
